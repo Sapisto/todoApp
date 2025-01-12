af359cc90c21a8b57a6eebf8461ba68209be1991
AlhassanAbd <alhassanabd@accessbankplc.com>
2025-01-12 18:07:07 +0100

    changes

--- PracticeApi/obj/Debug/net7.0/PracticeApi.AssemblyInfo.cs
+++ PracticeApi/obj/Debug/net7.0/PracticeApi.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("PracticeApi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+dcb9a32282dea9996b8269c72a99dbe0e937c658")]
 [assembly: System.Reflection.AssemblyProductAttribute("PracticeApi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("PracticeApi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
